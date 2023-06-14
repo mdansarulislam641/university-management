c84e823309977e265c0e2b653c58db8de3d5efdf
mdansarulislam641 <mdansarulislam641.com>
2023-06-14 07:55:06 +0600

    response rearrange for user friendly

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express'
 import { UserService } from './users.service'
 import catchAsync from '../../../shared/catchAsync'
+import sendResponse from '../../../shared/sendResponse'
 
 // create a new user
 const createANewUserDB: RequestHandler = catchAsync(
@@ -8,8 +9,11 @@ const createANewUserDB: RequestHandler = catchAsync(
     const userInfo = req.body
     const user = await UserService.createNewUser(userInfo)
     next()
-    res.status(200).json({
+
+    sendResponse(res, {
       success: true,
+      message: 'academic semester created successfully',
+      statusCode: 200,
       data: user,
     })
   }
