861e8527af4e99c12598dbde0d9e8f129ba62171
mdansarulislam641 <mdansarulislam641.com>
2023-06-23 08:42:43 +0600

    get all department

--- src/app/modules/academicDepertment/academicDepartment.controller.ts
+++ src/app/modules/academicDepertment/academicDepartment.controller.ts
@@ -3,6 +3,8 @@ import catchAsync from '../../../shared/catchAsync'
 import sendResponse from '../../../shared/sendResponse'
 import { IAcademicDepartment } from './academicDepartment.interface'
 import { academicDepartmentService } from './academicDepartment.service'
+import pick from '../../../shared/pick'
+import { paginationField } from '../../../constance/paginationField'
 
 // create a academic department controller
 const createDepartment = catchAsync(async (req: Request, res: Response) => {
@@ -19,12 +21,18 @@ const createDepartment = catchAsync(async (req: Request, res: Response) => {
 // get all academic Department
 const getAllAcademicDepartment = catchAsync(
   async (req: Request, res: Response) => {
-    const result = await academicDepartmentService.getAllAcademicDepartment()
+    const filters = pick(req.query, ['searchTerm', 'title'])
+    const paginationOptions = pick(req.query, paginationField)
+    const result = await academicDepartmentService.getAllAcademicDepartment(
+      filters,
+      paginationOptions
+    )
     sendResponse<IAcademicDepartment[]>(res, {
       statusCode: 200,
       success: true,
       message: 'Successfully get all academic department',
-      data: result,
+      meta: result.meta,
+      data: result.data,
     })
   }
 )
